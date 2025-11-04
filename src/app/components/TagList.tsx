a3044742cb7e380afc6163a71731b4d4e3ec3995
FreaKzero <pete@freakzero.com>
2025-11-04 13:19:26 +0100

    fix: cleanup tags component

--- src/app/components/TagList.tsx
+++ src/app/components/TagList.tsx
@@ -6,27 +6,29 @@ interface TagListProps {
   onClick?: (tag: string) => void;
 }
 
-interface TagPillProps extends React.HTMLAttributes<HTMLSpanElement> {
+interface TagPillProps {
   children: React.ReactNode;
-  fav: boolean;
-  pirate: boolean;
+  isFav: boolean;
+  isPirate: boolean;
+  onClick?: () => void;
 }
 
-export const TagPill: React.FC<TagPillProps> = ({ children, ...props }) => {
-  let color = 'border-green-400 bg-green-900/50 text-green-300 hover:bg-green-800/70 hover:text-green-200';
+const TagPill: React.FC<TagPillProps> = ({ children, isFav, isPirate, onClick }) => {
+  let color =
+    'border-green-400 bg-green-900/50 text-green-300 hover:bg-green-800/70 hover:text-green-200';
 
-  if (props.fav) {
-    color = 'border-yellow-400 bg-amber-900/50 text-yellow-300 hover:bg-amber-800/70 hover:text-yellow-200';
-  }
-
-  if (props.pirate) {
-    color = 'border-red-400 bg-red-900/50 text-red-300 hover:bg-red-800/70 hover:text-red-200';
+  if (isFav) {
+    color =
+      'border-yellow-400 bg-amber-900/50 text-yellow-300 hover:bg-amber-800/70 hover:text-yellow-200';
+  } else if (isPirate) {
+    color =
+      'border-red-400 bg-red-900/50 text-red-300 hover:bg-red-800/70 hover:text-red-200';
   }
 
   return (
     <span
-      {...props}
-      className={`inline-block rounded-full border  px-3 py-1 text-xs font-bold uppercase  cursor-pointer transition-colors duration-200 ${color}`}
+      onClick={onClick}
+      className={`inline-block rounded-full border px-3 py-1 text-xs font-bold uppercase cursor-pointer transition-colors duration-200 ${color}`}
     >
       {children}
     </span>
@@ -51,11 +53,11 @@ export const TagList: React.FC<TagListProps> = ({ tags, onClick }) => {
         return (
           <TagPill
             key={`tag-${i}`}
+            isFav={isFav}
+            isPirate={isPirate}
             onClick={() => onClick?.(tag)}
-            fav={isFav}
-            pirate={isPirate}
           >
-            {isFav ? <HeartIcon size='15' /> : isPirate ? <SkullIcon size='15' /> : tag}
+            {isFav ? <HeartIcon size={15} /> : isPirate ? <SkullIcon size={15} /> : tag}
           </TagPill>
         );
       })}
